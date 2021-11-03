473ae772f73827f69a8332f3ba8849d6ec69fd07
Carlos Gatti <eduardo.gf@hotmail.com>
2021-11-02 22:20:52 -0400

    Update ClearDB

--- infra/connectionFactory.js
+++ infra/connectionFactory.js
@@ -3,11 +3,10 @@ require('dotenv').config();
 
 function createDBConnection(){
   return mysql.createConnection({
-    host:process.env.DATABASE_URL,
-    user:process.env.DATABASE_USER, 
-    password:process.env.DATABASE_PASSWORD,
+    host:process.env.CLEARDB_DATABASE_URL,
+    //user:process.env.DATABASE_USER, 
+    //password:process.env.DATABASE_PASSWORD,
     //database:process.env.DATABASE,
-    database:process.env.CLEARDB_DATABASE_URL
   });
 }
 
