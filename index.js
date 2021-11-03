d1a0716b0219a365062188af55c6f7836b05808d
Carlos Gatti <eduardo.gf@hotmail.com>
2021-11-02 23:24:23 -0400

    Port 3006

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.set('Secret', config.secret);
 // use alternate localhost and the port Heroku assigns to $PORT
 const host = '0.0.0.0';
 //const host = 'localhost';
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 3006;
 
 app.listen(port, host, function() {
   console.log('Server running on the port ' + port);
